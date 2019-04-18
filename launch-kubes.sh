8687a28909342014e789e46bf0cd79e13eb6cd76
nicklotz <nlotzextemp@gmail.com>
2019-04-18 11:02:36 -0500

    Create launch-kubes.sh

---
+++ launch-kubes.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+
+echo "Configuring nested virtualization on ephemeral boot disk"
+gcloud compute disks create suse-virt-disk \
+  --image-project suse-cloud \
+  --image-family sles-15 \
+  --zone us-east1-b > /dev/null 2> /dev/null
+  
+echo "Creating peristent nested VM image"
+gcloud compute images create suse-virt-image \
+  --source-disk suse-virt-disk \
+  --source-disk-zone us-east1-b \
+  --licenses "https://www.googleapis.com/compute/v1/projects/vm-options/global/licenses/enable-vmx" > /dev/null 2> /dev/null
+  
+echo "Cleaning up ephemeral disks"
+gcloud compute disks delete suse-virt-disk \
+  --zone=us-east1-b \
+  --quiet > /dev/null 2> /dev/null
+  
+echo "Launching virtualization host"
+gcloud compute instances create kubernetes-host \
+  --zone us-east1-b \
+  --min-cpu-platform "Intel Haswell" \
+  --image suse-virt-image \
+  --boot-disk-size=500GB \
+  --custom-cpu=20 \
+  --custom-memory=75GB > /dev/null 2> /dev/null
+  
+echo "Configuring SSH on host"
+gcloud compute ssh kubernetes-host \
+  --zone=us-east1-b \
+  --force-key-file-overwrite \
+  --quiet > /dev/null 2> /dev/null
+
+sleep 30s
+
+echo "Installing KVM packages on host"
+bash kvm-prep.sh
