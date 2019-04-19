683216bfbb09d2786427e4856877d2d7ee201a7d
nicklotz <nlotzextemp@gmail.com>
2019-04-19 12:00:23 -0500

    Update kvm-prep.sh

--- kvm-prep.sh
+++ kvm-prep.sh
@@ -1,3 +1,14 @@
 #/bin/bash
 
-gcloud compute ssh kubernetes-host --zone=us-east1-b --command="sudo zypper ref; sudo zypper in -y -t pattern kvm_server kvm_tools"
+gcloud compute ssh kubernetes-host --zone=us-east1-b --command="sudo zypper ar http://download.opensuse.org/tumbleweed/repo/oss/ Tumbleweed" > /de
+v/null 2> /dev/null
+
+gcloud compute ssh kubernetes-host --zone=us-east1-b --command="sudo zypper --gpg-auto-import-keys ref; sudo zypper in -y -t pattern kvm_server kv
+m_tools" > /dev/null 2> /dev/null
+
+gcloud compute ssh kubernetes-host --zone=us-east1-b --command="sudo systemctl start libvirtd; sudo systemctl enable libvirtd" > /dev/null 2> /dev
+/null
+
+gcloud compute ssh kubernetes-host --zone=us-east1-b --command="sudo virsh net-list --all" > /dev/null 2> /dev/null
+
+gcloud compute ssh kubernetes-host --zone=us-east1-b --command="sudo virsh net-start default; sudo virsh net-autostart default"
