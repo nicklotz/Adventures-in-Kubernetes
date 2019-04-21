7a00b7855161b7c3f001441b004b7e840c1457d1
nicklotz <nlotzextemp@gmail.com>
2019-04-20 19:48:51 -0500

    Update kvm-install.sh

--- kvm-install.sh
+++ kvm-install.sh
@@ -2,24 +2,24 @@
 
 gcloud compute ssh kubernetes-host \
   --zone=us-east1-b \
-  --command="sudo zypper ar http://download.opensuse.org/tumbleweed/repo/oss/ Tumbleweed" &> /dev/null
-
+  --command="sudo zypper ar http://download.opensuse.org/tumbleweed/repo/oss/ Tumbleweed"
+  
 gcloud compute ssh kubernetes-host \
   --zone=us-east1-b \
-  --command="sudo zypper --gpg-auto-import-keys ref; sudo zypper in -y -t pattern kvm_server kvm_tools" &> /dev/null
-
+  --command="sudo zypper --gpg-auto-import-keys ref; sudo zypper in -y -t pattern kvm_server kvm_tools"
+  
 gcloud compute ssh kubernetes-host \
   --zone=us-east1-b \
-  --command="sudo systemctl start libvirtd; sudo systemctl enable libvirtd" &> /dev/null
-
-
+  --command="sudo systemctl start libvirtd; sudo systemctl enable libvirtd"
+  
 gcloud compute ssh kubernetes-host \
   --zone=us-east1-b \
-  --command="sudo virsh net-list --all" &> /dev/null
-
-
+  --command="sudo virsh net-list --all"
+  
 gcloud compute ssh kubernetes-host \
   --zone=us-east1-b \
   --command="sudo virsh net-start default; sudo virsh net-autostart default"
+  
+sleep 10s
 
 gcloud compute ssh kubernetes-host --zone=us-east1-b --command="sudo zypper in -y tftpboot-installation-openSUSE-MicroOS-x86_64"
