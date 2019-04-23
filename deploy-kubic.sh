8c332b8c80e9f07c1b38d186b527abe5321a35f4
nicklotz <nlotzextemp@gmail.com>
2019-04-23 11:30:27 -0500

    Update deploy-kubic.sh

--- deploy-kubic.sh
+++ deploy-kubic.sh
@@ -3,11 +3,19 @@
 gcloud compute ssh kubernetes-host \
   --zone=us-east1-b \
   --command="sudo systemctl enable --now tftp.socket; sudo systemctl start tftp.socket"
-
+  
 gcloud compute ssh kubernetes-host \
   --zone=us-east1-b \
   --command="sudo systemctl status tftp.socket"
-
+  
 gcloud compute ssh kubernetes-host \
   --zone=us-east1-b \
   --command="cd /srv/tftpboot/; sudo cp -av /usr/share/tftpboot-installation/openSUSE-MicroOS-x86_64/net/{message,pxelinux.*} ."
+  
+gcloud compute ssh kubernetes-host \
+  --zone=us-east1-b \
+  --command="cd /srv/tftpboot/; sudo cp -av /usr/share/tftpboot-installation/openSUSE-MicroOS-x86_64/boot/x86_64/root ."
+  
+gcloud compute scp pxelinux.cfg.default kubernetes-host:~/pxelinux.cfg.default --zone=us-east1-b
+
+gcloud compute ssh kubernetes-host --zone=us-east1-b --command="sudo mv ~/pxelinux.cfg.default /srv/tftpboot/pxelinux.cfg/default"
